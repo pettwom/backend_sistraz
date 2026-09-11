d6a8d6201cfd561c9b34b2b26dc2751eda512d4f
Petter Meave <pettertwom@gmail.com>
2026-09-11 15:14:11 -0400

    ajustes

--- Models/TrazabilidadDbContext.cs
+++ Models/TrazabilidadDbContext.cs
@@ -355,7 +355,9 @@ public partial class TrazabilidadDbContext : DbContext
             entity.Property(e => e.Usumod)
                 .HasComment("usuario de modificacion")
                 .HasColumnName("usumod");
-
+            entity.HasOne(d => d.Plantum).WithOne(p => p.IdNavigation)
+                .HasForeignKey<Produccion>(d => d.IdPlanta)
+                .HasConstraintName("produccion_id_planta_fkey");
             entity.HasOne(d => d.CorrelativoNavigation).WithMany(p => p.Produccions)
                 .HasForeignKey(d => d.Correlativo)
                 .HasConstraintName("fk_produccion_tbl_trazabilidad_0");
